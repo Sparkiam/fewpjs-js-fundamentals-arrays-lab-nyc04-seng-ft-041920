e4847cd1f692adeca058b8664a60cc3608373748
github username <github email address>
2020-06-21 18:58:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+
+//     expect(append).to.have.ordered.members(["Milo", "Otis", "Garfield", "Odie"]);
+
+const append = ['Milo', "Otis", 'Garfield'];
+append.push('Odie');
+append;
+// => ["Milo", "Otis", "Garfield", "Odie"]
+
+//
+
+// expect(prepend).to.have.ordered.members(["Odie", "Milo", "Otis", "Garfield"]);
+
+const prepend = ['Milo', "Otis", 'Garfield'];
+prepend.unshift('Odie');
+prepend;
+//  => ["Odie", "Milo", "Otis", "Garfield"]
+
+// 
+
+// expect(removeLast).to.have.ordered.members(["Milo", "Otis"]).and.to.not.include('Garfield');
+
+const removeLast = ['Milo', "Otis", 'Garfield'];
+removeLast.pop();
+removeLast;
+
+// expect(removeFirst).to.have.ordered.members(["Otis", "Garfield"]).and.to.not.include('Milo');
+
+const removeFirst = ['Milo', "Otis", 'Garfield'];
+removeFirst.shift();
+removeFirst;
